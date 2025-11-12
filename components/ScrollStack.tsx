d9b61769adeae1dd2201bbdf5c1d4b2900ed4a2d
Claytonwendel <clayton@thebettinginsider.com>
2025-11-12 08:58:57 -0500

    Fix TypeScript: Correct Lenis option name to gestureOrientation

--- components/ScrollStack.tsx
+++ components/ScrollStack.tsx
@@ -252,7 +252,7 @@ const ScrollStack = ({
         smoothWheel: true,
         touchMultiplier: 2,
         infinite: false,
-        gestureOrientationHandler: true,
+        gestureOrientation: 'vertical',
         normalizeWheel: true,
         wheelMultiplier: 1,
         touchInertiaMultiplier: 35,
